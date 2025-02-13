fa124f1baa8771b0d80bf407857c130cb4d3c169
PremBhooma <bhoomasagar1213@gmail.com>
2025-02-13 12:47:18 +0530

    zustand setup

---
+++ zustand/useUserInfo.js
@@ -0,0 +1,28 @@
+const { create } = require("zustand");
+import { persist, createJSONStorage } from "zustand/middleware";
+
+const store = () => ({
+  userInfo: null,
+  isLogged: false,
+  updateUserInfo: (data) => {
+    set({
+      userInfo: data,
+      isLogged: true,
+    });
+  },
+  resetUserInfo: () => {
+    set({
+      userInfo: null,
+      isLogged: false,
+    });
+  },
+});
+
+const useUserInfo = create(
+  persist(store, {
+    name: "userInfo",
+    store: createJSONStorage(() => sessionStorage),
+  })
+);
+
+export { useUserInfo };
