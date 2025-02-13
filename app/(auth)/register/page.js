e59d0f5f13cfbb962360f5b1e582f2e56e83d2ed
PremBhooma <bhoomasagar1213@gmail.com>
2025-02-13 17:53:40 +0530

    `Added import of useEffect and useUserInfo to register page.js`

--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useUserInfo } from "@/zustand/useUserInfo";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { BASEURL } from "@/configs/constant";
